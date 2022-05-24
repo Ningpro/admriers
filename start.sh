7ffb509b7c0cbd4f47d321b955c6d9db23e96aec
Code <64682175+Ningpro@users.noreply.github.com>
2022-05-25 04:36:11 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export LANG=en_US.UTF-8
+
+mkdir -p /etc/admirer
+version=`wget -qO- -t1 -T2 --no-check-certificate "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
+
+wget -q -O /etc/admirer/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/$version/hysteria-linux-amd64
+
+chmod 755 /etc/admirer/hysteria
+
+/etc/admirer/hysteria  server --config config.json
